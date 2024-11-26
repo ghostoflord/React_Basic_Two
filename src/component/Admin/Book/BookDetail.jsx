9eb5905b5c0f31fd6b8ab629dc502347cae11054
ghostoflord <ghost2892003zzz@gmail.com>
2024-11-26 16:22:46 +0700

    update view book

--- src/component/Admin/Book/BookDetail.jsx
+++ src/component/Admin/Book/BookDetail.jsx
@@ -1,14 +1,58 @@
-import { Badge, Descriptions, Drawer } from "antd";
+import { Badge, Descriptions, Divider, Drawer, Modal, Upload } from "antd";
 import moment from 'moment';
 import { FORMAT_DATE_DISPLAY } from "../../../util/constant";
+import { v4 as uuidv4 } from 'uuid';
+import { useState, useEffect } from "react";
 
 const BookDetail = (props) => {
     const { openViewDetail, setOpenViewDetail, dataViewDetail, setDataViewDetail } = props;
-
     const onClose = () => {
         setOpenViewDetail(false);
         setDataViewDetail(null);
     }
+    const [previewOpen, setPreviewOpen] = useState(false);
+    const [previewImage, setPreviewImage] = useState('');
+    const [previewTitle, setPreviewTitle] = useState('');
+    const [fileList, setFileList] = useState([]);
+
+    useEffect(() => {
+        if (dataViewDetail) {
+            let imgThumbnail = {}, imgSlider = [];
+            if (dataViewDetail.thumbnail) {
+                imgThumbnail = {
+                    uid: uuidv4(),
+                    name: dataViewDetail.thumbnail,
+                    status: 'done',
+                    url: `${import.meta.env.VITE_BACKEND_URL}/images/book/${dataViewDetail.thumbnail}`,
+                }
+            }
+            if (dataViewDetail.slider && dataViewDetail.slider.length > 0) {
+                dataViewDetail.slider.map(item => {
+                    imgSlider.push({
+                        uid: uuidv4(),
+                        name: item,
+                        status: 'done',
+                        url: `${import.meta.env.VITE_BACKEND_URL}/images/book/${item}`,
+                    })
+                })
+            }
+
+            setFileList([imgThumbnail, ...imgSlider])
+        }
+    }, dataViewDetail)
+
+    const handleCancel = () => setPreviewOpen(false);
+
+    const handlePreview = async (file) => {
+        setPreviewImage(file.url);
+        setPreviewOpen(true);
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+    };
+
+    const handleChange = ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+    }
+
     return (
         <>
             <Drawer
@@ -25,7 +69,7 @@ const BookDetail = (props) => {
                     <Descriptions.Item label="Id">{dataViewDetail?._id}</Descriptions.Item>
                     <Descriptions.Item label="Tiêu Đề">{dataViewDetail?.mainText}</Descriptions.Item>
                     <Descriptions.Item label="Tác Giả">{dataViewDetail?.author}</Descriptions.Item>
-                    <Descriptions.Item label="Giá Cả">{dataViewDetail?.price}</Descriptions.Item>
+                    <Descriptions.Item label="Giá tiền">{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(dataViewDetail?.price ?? 0)}</Descriptions.Item>
                     <Descriptions.Item label="Đã Bán">{dataViewDetail?.sold}</Descriptions.Item>
                     <Descriptions.Item label="Số Lượng">{dataViewDetail?.quantity}</Descriptions.Item>
                     <Descriptions.Item label="Loại Sách" span={2}>{dataViewDetail?.category}</Descriptions.Item>
@@ -36,6 +80,23 @@ const BookDetail = (props) => {
                         {moment(dataViewDetail?.updatedAt).format(FORMAT_DATE_DISPLAY)}
                     </Descriptions.Item>
                 </Descriptions>
+                <Divider orientation="left" > Ảnh Books </Divider>
+                <Upload
+                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={handleChange}
+                    showUploadList={
+                        { showRemoveIcon: false }
+                    }
+                >
+
+                </Upload>
+                <Modal open={previewOpen} title={previewTitle} footer={null} onCancel={handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+
             </Drawer>
         </>
     )
