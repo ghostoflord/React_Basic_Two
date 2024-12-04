7f4b0ed5b91034b8c14c7d025f22b9ee0f3a95d1
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-04 17:18:08 +0700

    update fix view

--- src/component/Home/home.jsx
+++ src/component/Home/home.jsx
@@ -5,6 +5,7 @@ import { Row, Col, Form, Checkbox, Divider, InputNumber, Button, Rate, Tabs, Pag
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './home.scss';
+import MobileFilter from './MobileFilter';
 const Home = () => {
 
     const [listCategory, setListCategory] = useState([]);
@@ -20,6 +21,8 @@ const Home = () => {
 
     const [form] = Form.useForm();
     const navigate = useNavigate();
+
+    const [showMobileFilter, setShowMobileFilter] = useState(false);
 
     useEffect(() => {
         const initCategory = async () => {
@@ -283,6 +286,16 @@ const Home = () => {
                                         onChange={(value) => { setSortQuery(value) }}
                                         style={{ overflowX: "auto" }}
                                     />
+                                    <Col xs={24} md={0}>
+                                        <div style={{ marginBottom: 20 }} >
+                                            <span onClick={() => setShowMobileFilter(true)}>
+                                                <FilterTwoTone />
+                                                <span style={{ fontWeight: 500 }}> Lọc</span>
+                                            </span>
+
+                                        </div>
+                                    </Col>
+                                    <br />
                                 </Row>
                                 <Row className='customize-row'>
                                     {listBook?.map((item, index) => {
@@ -320,8 +333,14 @@ const Home = () => {
                             </div>
                         </Spin>
                     </Col>
-
                 </Row>
+                <MobileFilter
+                    isOpen={showMobileFilter}
+                    setIsOpen={setShowMobileFilter}
+                    handleChangeFilter={handleChangeFilter}
+                    listCategory={listCategory}
+                    onFinish={onFinish}
+                />
             </div>
         </div>
     )
